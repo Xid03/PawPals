927d326b7545dfd3000760612c68ed43311e853d
xid03 <alongyzh@gmail.com>
2026-05-11 11:35:26 +0800

    Fix Vercel build setup

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { CommunityFeedClient } from "@/components/CommunityFeedClient";
 
 export default function CommunityFeedPage() {
-  return <CommunityFeedClient />;
+  return (
+    <Suspense>
+      <CommunityFeedClient />
+    </Suspense>
+  );
 }
